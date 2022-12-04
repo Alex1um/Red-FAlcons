d2dc74d7f318649dc4bb2b5dcb2df3a54c5dbe3f
John Doe <ulturgashev112@gmail.com>
2022-12-04 23:51:49 +0700

    add Card model

--- back/src/db/models.py
+++ back/src/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, text, TIMESTAMP
+from sqlalchemy import Column, ForeignKey, Integer, String, text, TIMESTAMP
 
 from .session import Base
 
@@ -9,6 +9,20 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    store_name = Column(String, nullable=False)
+    code = Column(Integer, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
