64351294c1f548d52cb41b3b0662a5af850d240e
egor <e.morozov@g.nsu.ru>
2022-12-05 23:16:28 +0700

    init test file

--- back/src/api/cards/api_cards_test.py
+++ back/src/api/cards/api_cards_test.py
@@ -7,19 +7,17 @@ import asyncio
 
 import pytest
 from fastapi.testclient import TestClient
-# from ...settings import settings
+from ...settings import settings
 from ...create_app import app
-from ...external.db.session import get_session
+from ...external.db.session import Base, get_session
+from ...external.db.models import Card, Store
 
-# DATABASE_URL = (
-#     f"postgresql+asyncpg://{settings.postgres_user}:"
-#     f"{settings.postgres_password}"
-#     f"@{settings.postgres_host}:"
-#     f"{settings.postgres_port}/{settings.postgres_database_name}_test"
-# )
-
-DATABASE_URL = "postgresql+asyncpg://admin:admin@127.0.0.1:5432/quickwallet_test"
-
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.postgres_user}:"
+    f"{settings.postgres_password}"
+    f"@{settings.postgres_host}:"
+    f"{settings.postgres_port}/{settings.postgres_database_name}_test"
+)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_test_session = sessionmaker(
@@ -30,8 +28,6 @@ async_test_session = sessionmaker(
     autocommit=False,
 )
 
-Base = declarative_base()
-
 
 async def get_test_session() -> AsyncSession:
     async with async_test_session() as session:
@@ -39,14 +35,14 @@ async def get_test_session() -> AsyncSession:
 
 app.dependency_overrides[get_session] = get_test_session
 
-# Base.metadata.create_all(bind=engine)
+
 async def init_models():
     async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-
 asyncio.run(init_models())
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
